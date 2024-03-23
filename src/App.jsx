89a2319d36192702d24d08375b02d5b4057c9fb1
Rik Baselier <r.baselier@student.fontys.nl>
2024-03-23 23:09:15 +0100

    working dropdown, needs customizing and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landingpage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <h1>Portfolio!</h1>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <h1>Portfolio!</h1>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
