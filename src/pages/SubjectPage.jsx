ccd80b0fd0dac3d87ebfed1cd45a238ff95c5dc5
Rik Baselier <r.baselier@student.fontys.nl>
2024-04-03 13:40:55 +0200

    subject page working

--- src/pages/SubjectPage.jsx
+++ src/pages/SubjectPage.jsx
@@ -7,127 +7,220 @@ import { imageUploader } from "../services/Firebase";
 const SubjectPage = () => {
   const { year, module, subject } = useParams();
   const [artPieces, setArtPieces] = useState([]);
-  const [werkPraktijkPieces, setWerkPraktijkPieces] = useState(null);
-  const [kennisVaardighedenPieces, setKennisVaardighedenPieces] =
-    useState(null);
-  const [positioneringPieces, setPositioneringPieces] = useState(null);
-
+  const [werkPraktijkPieces1, setWerkPraktijkPieces1] = useState([]);
+  const [werkPraktijkPieces2, setWerkPraktijkPieces2] = useState([]);
+  const [theoriePieces, setTheoriePieces] = useState([]);
+  const [skillsPieces, setSkillsPieces] = useState([]);
+  const [positioneringPieces, setPositioneringPieces] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     const getData = async () => {
-      const artPiecesData = await ArtPieceManager.getManyArtPieces({
-        year,
-        module,
-      });
-
-      setArtPieces(artPiecesData);
-      console.log(artPieces);
-
-      if (artPieces) {
-        switch (subject) {
-          case "werkpraktijk":
-            setWerkPraktijkPieces(
-              artPieces.filter(
-                (item) =>
-                  item.subject === "WERKPRAKTIJK_1" ||
-                  item.subject === "WERKPRAKTIJK_2",
-              ),
-            );
-            break;
-          case "kennis":
-            setKennisVaardighedenPieces(
-              artPieces.filter(
-                (item) =>
-                  item.subject === "THEORIE" || item.subject === "SKILLS",
-              ),
-            );
-            break;
-          case "positionering":
-            setPositioneringPieces(
-              artPieces.filter((item) => item.subject === "POSITIONERING"),
-            );
-            break;
-          default:
-            navigate("/");
-            break;
-        }
+      try {
+        const res = await ArtPieceManager.getManyArtPieces({ year, module });
+        setArtPieces(res.data);
+        console.log(artPieces);
+      } catch (error) {
+        console.error("Error fetching art pieces:", error);
+        // Handle error, maybe navigate to an error page
       }
     };
 
     getData();
-  }, []);
+  }, [year, module]); // Add dependencies for useEffect
+
+  // Filter art pieces when artPieces state updates
+  useEffect(() => {
+    console.log(artPieces);
+    console.log(werkPraktijkPieces1);
+    console.log(werkPraktijkPieces2);
+    console.log(theoriePieces);
+    console.log(skillsPieces);
+    console.log(positioneringPieces);
+    if (artPieces.length > 0) {
+      setWerkPraktijkPieces1(
+        artPieces.filter((item) => item.subject === "WERKPRAKTIJK_1"),
+      );
+      setWerkPraktijkPieces2(
+        artPieces.filter((item) => item.subject === "WERKPRAKTIJK_2"),
+      );
+      setTheoriePieces(artPieces.filter((item) => item.subject === "THEORIE"));
+      setSkillsPieces(artPieces.filter((item) => item.subject === "SKILLS"));
+      setPositioneringPieces(
+        artPieces.filter((item) => item.subject === "POSITIONERING"),
+      );
+    }
+  }, [artPieces]);
 
   return (
-    <div className="container px-5 py-6 mx-auto">
-      <div className="flex flex-wrap -m-4">
-        <a
-          className="p-4 md:w-1/3"
-          href={`/module/${year}/${module}/werkpraktijk`}
-        >
-          <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
-            <img
-              className="lg:h-48 md:h-36 w-full object-cover object-center"
-              src="/werkpraktijk.png"
-              alt="blog"
-            />
-            <div className="px-6 pb-2 pt-5">
-              <h2 className="text-xs title-font font-medium text-gray-400">
-                Jaar {year}, M{module}
-              </h2>
-              <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
-                Werkpraktijk
-              </h1>
-              <p className="leading-relaxed mb-3 text-gray-600">
-                Werkpraktijk 1 en Werkprakijk 2
-              </p>
-            </div>
+    <div className="container px-5 py-2 mx-auto">
+      {subject === "werkpraktijk" && (
+        <>
+          <h2 className="mx-auto w-fit font-light">Werkpraktijk 1</h2>
+          <div
+            className="w-1/5 bg-[#b5bab6] mx-auto landingline mb-5"
+            height={2}
+          />
+          <div className="flex flex-wrap -m-4">
+            {/* Render pieces for Werkpraktijk 1 */}
+            {werkPraktijkPieces1.map((piece) => (
+              <div className="p-4 md:w-1/3" key={piece.id}>
+                <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
+                  <img
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src={getDownloadURL(
+                      ref(imageUploader, piece.media[0].locationReference),
+                    )}
+                  />
+                  <div className="px-6 pb-2 pt-5">
+                    {/* <h2 className="text-xs title-font font-medium text-gray-400">
+                      {piece.title}
+                    </h2> */}
+                    <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
+                      {piece.title}
+                    </h1>
+                    <p className="leading-relaxed mb-3 text-gray-600">
+                      {piece.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        </a>
-        <a className="p-4 md:w-1/3" href={`/module/${year}/${module}/kennis`}>
-          <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
-            <img
-              className="lg:h-48 md:h-36 w-full object-cover object-center"
-              src="/kennisvaardigheden.jpg"
-              alt="blog"
-            />
-            <div className="px-6 pb-2 pt-5">
-              <h2 className="text-xs title-font font-medium text-gray-400">
-                Jaar {year}, M{module}
-              </h2>
-              <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
-                Kennis en Vaardigheden
-              </h1>
-              <p className="leading-relaxed mb-3 text-gray-600">
-                Theorie en Skills
-              </p>
-            </div>
+          <h2 className="mx-auto w-fit font-light">Werkpraktijk 2</h2>
+          <div
+            className="w-1/5 bg-[#b5bab6] mx-auto landingline mb-5"
+            height={2}
+          />
+          <div className="flex flex-wrap -m-4">
+            {/* Render pieces for Werkpraktijk 2 */}
+            {werkPraktijkPieces2.map((piece) => (
+              <div className="p-4 md:w-1/3" key={piece.id}>
+                <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
+                  <img
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src={getDownloadURL(
+                      ref(imageUploader, piece.media[0].locationReference),
+                    )}
+                  />
+                  <div className="px-6 pb-2 pt-5">
+                    {/* <h2 className="text-xs title-font font-medium text-gray-400">
+                    {piece.title}
+                  </h2> */}
+                    <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
+                      {piece.title}
+                    </h1>
+                    <p className="leading-relaxed mb-3 text-gray-600">
+                      {piece.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        </a>
-        <a
-          className="p-4 md:w-1/3"
-          href={`/module/${year}/${module}/positionering`}
-        >
-          <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
-            <img
-              className="lg:h-48 md:h-36 w-full object-cover object-center"
-              src="/positionering.jpg"
-              alt="blog"
-            />
-            <div className="px-6 pb-2 pt-5">
-              <h2 className="text-xs title-font font-medium text-gray-400">
-                Jaar {year}, M{module}
-              </h2>
-              <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
-                Positionering
-              </h1>
-              <p className="leading-relaxed mb-3 text-gray-600">
-                Positionering
-              </p>
-            </div>
+        </>
+      )}
+      {subject === "kennis" && (
+        <>
+          <h2 className="mx-auto w-fit font-light">Theorie</h2>
+          <div
+            className="w-1/5 bg-[#b5bab6] mx-auto landingline mb-5"
+            height={2}
+          />
+          <div className="flex flex-wrap -m-4">
+            {/* Render pieces for Werkpraktijk 1 */}
+            {theoriePieces.map((piece) => (
+              <div className="p-4 md:w-1/3" key={piece.id}>
+                <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
+                  <img
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src={getDownloadURL(
+                      ref(imageUploader, piece.media[0].locationReference),
+                    )}
+                  />
+                  <div className="px-6 pb-2 pt-5">
+                    {/* <h2 className="text-xs title-font font-medium text-gray-400">
+                    {piece.title}
+                  </h2> */}
+                    <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
+                      {piece.title}
+                    </h1>
+                    <p className="leading-relaxed mb-3 text-gray-600">
+                      {piece.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        </a>
-      </div>
+          <h2 className="mx-auto w-fit font-light">Skills</h2>
+          <div
+            className="w-1/5 bg-[#b5bab6] mx-auto landingline mb-5"
+            height={2}
+          />
+          <div className="flex flex-wrap -m-4">
+            {/* Render pieces for Werkpraktijk 2 */}
+            {skillsPieces.map((piece) => (
+              <div className="p-4 md:w-1/3" key={piece.id}>
+                <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
+                  <img
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src={getDownloadURL(
+                      ref(imageUploader, piece.media[0].locationReference),
+                    )}
+                  />
+                  <div className="px-6 pb-2 pt-5">
+                    {/* <h2 className="text-xs title-font font-medium text-gray-400">
+                    {piece.title}
+                  </h2> */}
+                    <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
+                      {piece.title}
+                    </h1>
+                    <p className="leading-relaxed mb-3 text-gray-600">
+                      {piece.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+      {subject === "positionering" && (
+        <>
+          <h2 className="mx-auto w-fit font-light">Positionering</h2>
+          <div
+            className="w-1/5 bg-[#b5bab6] mx-auto landingline mb-5"
+            height={2}
+          />
+          <div className="flex flex-wrap -m-4">
+            {/* Render pieces for Positionering */}
+            {positioneringPieces.map((piece) => (
+              <div className="p-4 md:w-1/3" key={piece.id}>
+                <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
+                  <img
+                    className="lg:h-48 md:h-36 w-full object-cover object-center"
+                    src={getDownloadURL(
+                      ref(imageUploader, piece.media[0].locationReference),
+                    )}
+                  />
+                  <div className="px-6 pb-2 pt-5">
+                    {/* <h2 className="text-xs title-font font-medium text-gray-400">
+                    {piece.title}
+                  </h2> */}
+                    <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
+                      {piece.title}
+                    </h1>
+                    <p className="leading-relaxed mb-3 text-gray-600">
+                      {piece.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
