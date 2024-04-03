3ba0adb93b8d5743bc5f91a9174c7eb4054e39d2
Rik Baselier <r.baselier@student.fontys.nl>
2024-04-03 14:16:09 +0200

    fix image rendering on subjectPage and utilize PortfolioItemCard to minimize subjectPage code

--- src/components/PortfolioItemCard.jsx
+++ src/components/PortfolioItemCard.jsx
@@ -1,10 +1,19 @@
 const PortfolioItem = (props) => {
   return (
-    <div className="border-solid rounded border-black border-2 flex w-full">
-      <img src={props.image} />
-      <div className="p-2 w-full">
-        <h1 className="font-bold">{props.title}</h1>
-        {props.description}
+    <div className="p-4 md:w-1/3" key={props.piece.id}>
+      <div className="h-full border-2 border-gray-300 border-opacity-60 rounded-lg overflow-hidden">
+        <img
+          className="lg:h-56 md:h-44 w-full object-cover object-center"
+          src={props.piece.downloadUrl}
+        />
+        <div className="px-6 pb-2 pt-5">
+          <h1 className="title-font text-lg font-semibold text-gray-800 mb-3">
+            {props.piece.title}
+          </h1>
+          <p className="leading-relaxed mb-3 text-gray-600">
+            {props.piece.description}
+          </p>
+        </div>
       </div>
     </div>
   );
