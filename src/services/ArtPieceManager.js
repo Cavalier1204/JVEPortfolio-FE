429f57ff12af368c16aaa971adc7a6324aa92dbc
Rik Baselier <r.baselier@student.fontys.nl>
2024-05-31 12:11:03 +0200

    Implement changes from API v1.0.1

--- src/services/ArtPieceManager.js
+++ src/services/ArtPieceManager.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const publicUrl = `${process.env.REACT_APP_API_LINK}/public/artpiece`;
-const protectedUrl = `${process.env.REACT_APP_API_LINK}/api/artpiece`;
+const publicUrl = `${process.env.REACT_APP_API_LINK}/public`;
+const protectedUrl = `${process.env.REACT_APP_API_LINK}/api`;
 
 // Get one
 const getArtPiece = async (id) => {
@@ -11,14 +11,38 @@ const getArtPiece = async (id) => {
 };
 
 // Get by year by module
-const getManyArtPieces = async ({ year, module }) => {
+const getArtPiecesByYearByModule = async ({ year, module }) => {
   return await axios
-    .get(publicUrl, {
+    .get(`${publicUrl}/module`, {
       params: {
         year: parseInt(year),
         module: parseInt(module),
       },
     })
+    .then((response) => response.data);
+};
+
+// Get by year by module by subject
+const getArtPiecesByYearByModuleBySubject = async ({
+  year,
+  module,
+  subject,
+}) => {
+  return await axios
+    .get(`${publicUrl}/subject`, {
+      params: {
+        year: parseInt(year),
+        module: parseInt(module),
+        subject: subject,
+      },
+    })
+    .then((response) => response.data);
+};
+
+// Get portfolio items
+const getPortfolioPieces = async () => {
+  return await axios
+    .get(`${publicUrl}/portfolio`)
     .then((response) => response.data);
 };
 
@@ -44,7 +68,9 @@ const deleteArtPiece = async (id, accessToken) => {
 
 export default {
   getArtPiece,
-  getManyArtPieces,
+  getArtPiecesByYearByModule,
+  getArtPiecesByYearByModuleBySubject,
+  getPortfolioPieces,
   saveArtPiece,
   deleteArtPiece,
 };
